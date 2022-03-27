37f0d380108fc60ce1b673ac625bef30b0e8b93f
LSCube <65765136+LSCube7@users.noreply.github.com>
2022-03-27 16:58:46 +0800

    0.1.0-pre1

--- dll/minimize/main.cpp
+++ dll/minimize/main.cpp
@@ -1,10 +1,54 @@
 #include <iostream>
 #include <Windows.h>
+#include <set>
+#include <vector>
+using namespace std;
 
 int cx = GetSystemMetrics(SM_CXFULLSCREEN);
 int cy = GetSystemMetrics(SM_CYFULLSCREEN) + GetSystemMetrics(SM_CYCAPTION);
 
-__declspec (dllexport) void minimize() {
-	HWND hwnd = GetForegroundWindow();
+set<string>ss;
+string se;
+
+BOOL CALLBACK EnumChildProc(_In_ HWND   hwnd, _In_ LPARAM lParam) {
+	char szTitle[MAX_PATH] = { 0 };
+	char szClass[MAX_PATH] = { 0 };
+	int nMaxCount = MAX_PATH;
+	LPSTR lpClassName = szClass;
+	LPSTR lpWindowName = szTitle;
+	GetWindowTextA(hwnd, lpWindowName, nMaxCount);
+	GetClassNameA(hwnd, lpClassName, nMaxCount);
+	string l = lpWindowName, r = lpClassName;
+	if (ss.count(l))return TRUE;
+	ss.insert(l);
+	if (l.size() == 0)return TRUE;
+	return TRUE;
+}
+
+BOOL CALLBACK EnumWindowsProc(HWND hwnd, LPARAM lParam) {
+	char szTitle[MAX_PATH] = { 0 };
+	char szClass[MAX_PATH] = { 0 };
+	int nMaxCount = MAX_PATH;
+	LPSTR lpClassName = szClass;
+	LPSTR lpWindowName = szTitle;
+	GetWindowTextA(hwnd, lpWindowName, nMaxCount);
+	GetClassNameA(hwnd, lpClassName, nMaxCount);
+	string l = lpWindowName, r = lpClassName;
+	if (ss.count(l))return TRUE;
+	ss.insert(l);
+	if (l.size() == 0)return TRUE;
+	EnumChildWindows(hwnd, EnumChildProc, lParam);
+	return TRUE;
+}
+
+void search(string s) {
+	se = s;
+	ss.clear();
+	EnumWindows(EnumWindowsProc, 0);
+	HWND hwnd{};
 	SetWindowPos(hwnd, HWND_NOTOPMOST, 0, 0, cx, cy, SWP_NOSENDCHANGING);
+}
+
+__declspec (dllexport) void minimize() {
+	search("Classroom_");
 };
